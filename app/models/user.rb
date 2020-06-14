c47bccb817685e59f8745dc7f106b22ea9c150bb
JijiKusahara <kayokawase0519@gmail.com>
2020-06-14 16:28:10 +0900

    Edit Groups
    Create New group function
    Edit group function
    
    end the Branch misstook Corrected
    side-ber was too many width when make a flash .

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
-
+  #cariduramu 4324 de kaita. 4266 dehanakunatteta
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
